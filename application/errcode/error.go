ff34a046f181e4c771a9e2fd02ebada0c836c7ea
wzy <hnwzy@live.com>
2025-11-13 16:12:08 +0800

    fix: 提交时间

--- application/errcode/error.go
+++ application/errcode/error.go
@@ -5,5 +5,5 @@ import "github.com/xgfone/ship/v5"
 var (
 	ErrNilDocument            = ship.ErrBadRequest.Newf("数据不存在")
 	ErrCertificateInvalid     = ship.ErrBadRequest.Newf("无效证书")
-	ErrCertificateUnavailable = ship.ErrBadRequest.Newf("未配置有效的证书")
+	ErrCertificateUnavailable = ship.ErrBadRequest.Newf("未配置任何的证书")
 )
