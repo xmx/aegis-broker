1c9c8bf2b534667a1073cb1f34b8c74b026363f6
xmx <no@example.com>
2025-11-26 21:06:26 +0800

    update

--- config/config.go
+++ config/config.go
@@ -1,8 +1,8 @@
 package config
 
 type Config struct {
-	Secret    string   `json:"secret"    validate:"required,lte=1000"`
-	Protocols []string `json:"protocols" validate:"omitempty,lte=4,unique,dive,oneof=tcp udp udp-quic-std udp-quic-go"`
-	Addresses []string `json:"addresses" validate:"lte=100"`
-	Offset    int64    `json:"offset"`
+	Secret    string   `json:"secret,omitzero"    validate:"required,lte=1000"`
+	Protocols []string `json:"protocols,omitzero" validate:"omitempty,lte=4,unique,dive,oneof=tcp udp udp-quic-std udp-quic-go"`
+	Addresses []string `json:"addresses,omitzero" validate:"lte=100"`
+	Offset    int64    `json:"offset,omitzero"`
 }
